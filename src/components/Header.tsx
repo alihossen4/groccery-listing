89fc1a6f35394412ceff3257bf873df6280a720f
alihossen4 <mdahossen.dev@gmail.com>
2026-05-16 01:09:36 +0600

    fealt: Navbar added and search

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,18 +3,44 @@ import React from 'react'
 import logo from "../../public/logo.png"
 import Link from 'next/link'
 import Search from './Search'
+import {FaRegHeart} from "react-icons/fa"
+import {IoBagOutline} from "react-icons/io5"
+import { IoCartOutline } from "react-icons/io5";
+import Navbar from './Navbar'
 const Header = () => {
   return (
-    <header>
-        <div className="container flex items-center justify-between py-4">
-            <div className="logo">
-                <Link href={"/"}>
-                    <Image src={logo} alt='logo' width={200} height={70}/>
-                </Link>
+    <>
+    <div className="header">
+        <header>
+            <div className="container flex items-center justify-between py-0">
+                <div className="logo">
+                    <Link href={"/"}>
+                        <Image src={logo} alt='logo' width={200} height={40}/>
+                    </Link>
+                </div>
+                <Search />
+                <div className="flex items-center gap-5">
+                    <div className="flex items-center gap-2">
+                        <Link href={"/login"} className='text-white px-4 py-1.5 rounded-md bg-primary hover:bg-green-300 hover:text-black'>Login</Link>
+                        <Link href={"/signup"} className='text-white ml-4 px-4 py-1.5 rounded-md bg-blue-500 hover:bg-blue-300 hover:text-black'>Sign Up</Link>
+                        </div>
+                    <div className="flex items-center ml-3">
+                        <Link href={"/favorite"} className="relative mr-3 group">
+                            <span className='bg-[#DC2626] flex absolute -top-2 -right-2 w-5 h-5 items-center justify-center text-white text-xs rounded-full text-md'>3</span>
+                            <FaRegHeart size={35} className='cursor-pointer group-hover:text-primary'/>
+                        </Link>
+                        <Link href={"/cart-items"} className="relative group">
+                            <span className='bg-[#DC2626] flex absolute -top-1.5 -right-2 w-5 h-5 items-center justify-center text-white text-xs rounded-full text-md'>5</span>
+                            <IoCartOutline size={37} className='cursor-pointer group-hover:text-primary'/>
+                        </Link>
+                    </div>
+                </div>
             </div>
-            <Search />
-        </div>
-    </header>
+        </header>
+        <hr className='text-gray-300'/>
+        <Navbar/>
+    </div>
+    </>
   )
 }
 
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-
+import {Button} from '@mui/material'
+import {IoSearchOutline} from "react-icons/io5"
 const Search = () => {
   return (
-    <div className='search bg-[#e6e6e6] w-[600px] h-[50px] rounded-md px-3'>
-        <input type="text" className='w-full h-full outline-none border-0'/>
+    <div className='search bg-[#e6e6e6] w-[600px] h-[50px] rounded-md px-3 relative flex items-center border border-[rgba(0,0,0,0.1)] hover:border-[rgba(0,0,0,0.3)]'>
+        <input type="text" className='w-full h-full outline-none border-0 py-4' placeholder='Search for products...'/>
+        <button className='h-10 w-10 cursor-pointer text-gray-500 absolute top-[5px] right-3 rounded-full hover:bg-gray-300 absolute  items-center flex justify-center'>
+            <IoSearchOutline size={35}/>
+        </button>
     </div>
   )
 }
